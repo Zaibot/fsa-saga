a8031b21403a3fa01168a059e0f89612ef44d71b
Tobias de Groen <tdgroen@zaibot.net>
2017-08-31 21:46:54 +0200

    fix(transform): verify is action type is factory

--- src/transform.ts
+++ src/transform.ts
@@ -3,10 +3,17 @@ import * as effects from 'redux-saga/effects';
 import { ActionType } from './ActionType';
 import { Pattern } from './Pattern';
 
+function transformAction<TPayload, TMeta>(action: ActionType<TPayload, TMeta>): effects.ActionType {
+    if (typeof action === 'function' && typeof action.type === 'string') {
+        return action.type as effects.ActionType;
+    }
+    throw Error(`expected function with type property`);
+}
+
 export function transform(actions: Pattern): effects.Pattern {
     if (actions instanceof Array) {
-        return actions.map((action: ActionType) => action.type) as effects.ActionType[];
+        return actions.map(transformAction) as effects.ActionType[];
     } else {
-        return actions.type as effects.ActionType;
+        return transformAction(actions);
     }
 }
